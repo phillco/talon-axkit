725999791a00fc6ce0cfce1d2ead1d11d16db89f
Nicholas Riley <com-github@sabi.net>
2022-02-20 13:41:08 -0500

    notification: Allow addressing notifications by app name.
    Also work around knausj issue in which making a spoken form of “don’t”…doesn’t.
    
    If rich is installed, prints more colorful log output (thanks @phillco!)
    
    Still running into a problem where sometimes the lists don't seem to update.  Not sure if I am doing something wrong or this is a Talon bug.

--- notification.py
+++ notification.py
@@ -1,28 +1,52 @@
 from talon import actions, app, cron, ui, Context, Module
 
 from dataclasses import dataclass, field
+from itertools import chain
+from typing import Optional
 
 # XXX actions are being returned out of order; that's a problem if we want to pop up a menu
 
 mod = Module()
 
 mod.list('notification_actions', desc='Notification actions')
+mod.list('notification_apps', desc='Notification apps')
+
+try:
+	from rich.console import Console
+
+	def print(obj: any, *args):
+		"""Pretty prints the object"""
+		console = Console(color_system="truecolor", soft_wrap=True)
+		if args:
+			console.out(obj, *args)
+		else:
+			console.print(obj)
+except ImportError:
+	pass
 
 @mod.action_class
 class Actions:
 	def notification_action(index: int, action: str) -> bool:
-		"""Perform the specified action on the notification at the specified index"""
+		"""Perform the specified action on the notification (stack) at the specified index"""
+		return False
+
+	def notification_app_action(app_name: str, action: str) -> bool:
+		"""Perform the specified action on the first notification (stack) for the specified app"""
 		return False
 
 	def notifications_update():
 		"""Update notification list to reflect what is currently onscreen"""
 		# (poll? not try to keep up? not sure what else to do)
 
+	def notifications_lists():
+		"""XXX debugging - remove"""
+
 @dataclass(frozen=True)
 class Notification:
 	identifier: int
 	subrole: str = field(default=None, compare=False)
-	stackingIdentifier: str = field(default=None, compare=False)
+	app_name: str = field(default=None, compare=False)
+	stacking_identifier: str = field(default=None, compare=False)
 	title: str = field(default=None, compare=False)
 	body: str = field(default=None, compare=False)
 	# action values are named "Name:<name>\nTarget:0x0\nSelector:(null)"; keys are speakable
@@ -39,11 +63,11 @@ class Notification:
 
 	@staticmethod
 	def from_group(group, identifier):
-		stackingIdentifier = getattr(group, 'AXStackingIdentifier')
-		subrole = getattr(group, 'AXSubrole')
 		actions = group.actions
 		if 'AXScrollToVisible' in actions: del actions['AXScrollToVisible'] # not useful
-		actions = dict((name.lower(), action) for action, name in actions.items())
+		# XXX create_spoken_forms_from_list doesn't handle apostrophes correctly
+		# https://github.com/knausj85/knausj_talon/issues/780
+		actions = {name.lower().replace("’","'"): action for action, name in actions.items()}
 
 		title = None
 		try: title = group.children.find_one(AXIdentifier='title').AXValue
@@ -55,8 +79,9 @@ class Notification:
 
 		return Notification(
 			identifier=identifier,
-			subrole=subrole,
-			stackingIdentifier=stackingIdentifier,
+			subrole=group.AXSubrole,
+			app_name=group.AXDescription,
+			stacking_identifier=group.AXStackingIdentifier,
 			title=title,
 			body=body,
 			actions=actions)
@@ -82,14 +107,24 @@ os: mac
 """
 
 ctx.lists['user.notification_actions'] = {}
+ctx.lists['user.notification_apps'] = {}
 
 @ctx.action_class('user')
 class UserActions:
 	def notification_action(index: int, action: str) -> bool:
-		return MONITOR.perform_action(index, action)
+		return MONITOR.perform_action(action, index=index)
+
+	def notification_app_action(app_name: str, action: str) -> bool:
+		return MONITOR.perform_action(action, app_name=app_name)
 
 	def notifications_update():
 		MONITOR.update_notifications()
+
+	def notifications_lists():
+		print(ctx.lists)
+		from talon import registry
+		print(registry.lists['user.notification_actions'])
+		print(registry.lists['user.notification_apps'])
 
 class NotificationMonitor(object):
 	__slots__ = (
@@ -124,21 +159,32 @@ class NotificationMonitor(object):
 
 				yield identifier, group
 
-	def perform_action(self, index: int, action: str):
+	def perform_action(self, action: str, index: Optional[int] = None, app_name: str = None):
 		self.update_notifications()
 
 		cron.after('500ms', self.update_notifications)
-		if index < 0 or index > len(self.notifications) - 1:
-			app.notify(f'Unable to locate notification #{index}', 'Try again?')
-			return False
 
-		notification = self.notifications[index]
+		notification = None
+		if index is not None:
+			if index < 0 or index > len(self.notifications) - 1:
+				app.notify(f'Unable to locate notification #{index}', 'Try again?')
+				return False
+
+			notification = self.notifications[index]
+		elif app_name is not None:
+			try:
+				notification = next(
+					notification for notification in self.notifications
+					if notification.app_name == app_name)
+			except StopIteration:
+				app.notify(f'Unable to locate notification for {app_name}', 'Try again?')
+				return False
 
 		for identifier, group in self.notification_groups():
 			if identifier != notification.identifier:
 				continue
 
-			print('performing', action, 'in', notification.actions)
+			print(f'performing {action} in {notification.actions}')
 
 			if action not in notification.actions:
 				# allow closing a notification stack like an individual notification
@@ -169,18 +215,42 @@ class NotificationMonitor(object):
 				notifications[y] = Notification.from_group(group, identifier)
 
 		self.notifications = list(notifications.values())
-		print('notifications:', notifications)
+		print(notifications)
 
 		notification_actions = set()
+		notification_apps = set()
+
 		for notification in self.notifications:
 			notification_actions.update(notification.actions.keys())
+			notification_apps.add(notification.app_name)
+
 		notification_actions = list(notification_actions)
-		notification_actions = actions.user.create_spoken_forms_from_list(notification_actions)
-		print('actions:', notification_actions)
+		# XXX create_spoken_forms_from_list doesn't handle apostrophes correctly
+		# https://github.com/knausj85/knausj_talon/issues/780
+		apostrophe_words = {word.replace("'", " "): word for word in
+							chain.from_iterable(action.split() for action in notification_actions)
+							if "'" in word}
+		words_to_exclude = [word.split(" ")[0] for word in apostrophe_words]
+		notification_actions = actions.user.create_spoken_forms_from_list(
+			notification_actions, words_to_exclude=words_to_exclude)
+		notification_actions = {
+			spoken_form.replace(mangled_word, word): action
+			for mangled_word, word in apostrophe_words.items()
+			for spoken_form, action in notification_actions.items()
+			if 'apostrophe' not in spoken_form}
+		print('actions', notification_actions)
+
 		if 'close' not in notification_actions and 'clear all' in notification_actions:
 			# allow closing a notification stack like an individual notification
 			notification_actions['close'] = 'clear all'
 		ctx.lists['user.notification_actions'] = notification_actions
+
+		# XXX use app name overrides from knausj?
+		notification_apps = actions.user.create_spoken_forms_from_list(notification_apps)
+		ctx.lists['user.notification_apps'] = notification_apps
+		print('apps', notification_apps)
+
+		# XXX lists fail randomly, not sure why
 
 	def win_close(self, window):
 		if not window.app.pid == self.pid:
--- notification.talon
+++ notification.talon
@@ -6,5 +6,15 @@ os: mac
 ^(note | notification) {user.notification_actions} <number_small>$:
 	user.notification_action(number_small - 1, notification_actions)
 
+^(note | notification) {user.notification_actions} {user.notification_apps}$:
+	user.notification_app_action(notification_apps, notification_actions)
+
+^(note | notification) {user.notification_apps} {user.notification_actions}$:
+	user.notification_app_action(notification_apps, notification_actions)
+
 ^(note | notification) update$:
 	user.notifications_update()
+
+# debugging
+^(note | notification) lists$:
+	user.notifications_lists()
