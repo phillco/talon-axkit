6911f9bfdb2497cb3c99ae3c6a402f721afa7335
Phillip Cohen <phillip@phillip.io>
2022-03-19 10:54:58 -0700

    dictation_context: accessibility context for dictation mode

---
+++ dictation/dictation_context.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from talon import Context, ui, Module, actions
+from talon.mac.ui import Element
+from talon.types import Span
+
+ctx = Context()
+ctx.matches = "os: mac"
+
+mod = Module()
+setting_accessibility_dictation = mod.setting(
+    "accessibility_dictation",
+    type=bool,
+    default=False,
+    desc="Use accessibility APIs to implement context aware dictation.",
+)
+
+# Default number of characters to use to acquire context. Somewhat arbitrary.
+# The current dictation formatter doesn't need very many, but that could change in the future.
+DEFAULT_CONTEXT_CHARACTERS = 30
+
+
+@dataclass
+class AccessibilityContext:
+    """Records the context needed for dictation"""
+    content: str
+    selection: Span
+
+    def left_context(self, num_chars: int = DEFAULT_CONTEXT_CHARACTERS) -> str:
+        """Returns `num_chars`' worth of context to the left of the cursor"""
+        start = max(0, self.selection.left - num_chars)
+        return self.content[start:self.selection.left]
+
+    def right_context(self, num_chars: int = DEFAULT_CONTEXT_CHARACTERS) -> str:
+        """Returns `num_chars`' worth of context to the right of the cursor"""
+        end = min(self.selection.right + num_chars, len(self.content))
+        return self.content[self.selection.right:end]
+
+
+@mod.action_class
+class ModActions:
+
+    def should_use_accessibility_dictation():
+        """Returns whether the"""
+        return setting_accessibility_dictation.get()
+
+    def accessibility_adjust_context_for_application(el: Element,
+                                                     context: AccessibilityContext) -> AccessibilityContext:
+        """Sometimes the accessibility context reported by the application is wrong, but fixable
+        in predictable ways (this is most common in electron apps).
+        
+        This method can be overwritten in those applications to do so.
+        """
+        return context
+
+    def accessibility_create_context(el: Element) -> Optional[AccessibilityContext]:
+        """Peeks the context in the given direction"""
+        if not actions.user.should_use_accessibility_dictation():
+            return None
+
+        if not el or not el.attrs:
+            # No accessibility support.
+            return None
+
+        context = AccessibilityContext(content=el.get("AXValue"), selection=el.AXSelectedTextRange)
+
+        # Support application-specific overrides:
+        context = actions.user.accessibility_adjust_context_for_application(el, context)
+
+        return context
+
+
+@ctx.action_class("self")
+class Actions:
+    """Wires this in to the knausj dictation formatter"""
+
+    def dictation_peek_left(clobber=False):
+        context = actions.user.accessibility_create_context(ui.focused_element())
+        if context is None:
+            return actions.next()
+
+        return context.left_context()
+
+    def dictation_peek_right():
+        context = actions.user.accessibility_create_context(ui.focused_element())
+        if context is None:
+            return actions.next()
+
+        return context.right_context()
