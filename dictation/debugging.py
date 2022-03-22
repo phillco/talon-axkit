5644cba344756eb326e9b5737a9ad02990549f94
Phil Cohen <phillip@phillip.io>
2022-03-21 21:38:52 -0700

    accessibility dictation: first round (#9)
    * dictation_context: accessibility context for dictation mode
    
    * dictation_context: naming tweaks
    
    * electron: add enable_electron_accessibility
    
    * dictation_context: gaurd range
    
    * dictation_context: handle missing selection range
    
    * dictation_context: return None if both content and range are empty
    
    * electron: check in support for enabling accessibility in electron
    
    * dictation_context: wrap peek in try/catch so we will fall back to knausj methods on failure
    
    * dictation_context: dictation_current_element() so contexts can choose the different element to serve as the input buffer
    
    * dictation_context: add override for Messages, to support empty buffers properly
    
    * dictation_context: improve messages printed during an exception
    
    * debugging: add hissing to debug accessibility, using the `hiss_to_debug_accessibility` setting
    
    * electron: rename setting_electron_accessibility
    
    * debugging: remove extra rule
    
    * dictation_context: improve comment
    
    * messages: fix action class
    
    * debugging, dictation_context, electron: code review fixes
    
    * dictation_context: shortened docstrings
    
    * dictation_context: enum
    
    * dictation_context, electron: review comments

---
+++ dictation/debugging.py
@@ -0,0 +1,94 @@
+import time
+import traceback
+
+from talon import Module, actions, cron, noise, ui
+from talon.mac.ui import Element
+
+HISS_DEBUG_ENABLED = True
+
+mod = Module()
+setting_enabled = mod.setting(
+    "hiss_to_debug_accessibility",
+    type=bool,
+    default=False,
+    desc="Use a hissing sound to print accessibility debugging information to the Talon log.",
+)
+setting_threshold = mod.setting(
+    "hiss_to_debug_accessibility_threshold",
+    type=float,
+    default=0.35,
+    desc="If hiss_to_debug_accessibility is enabled, the hissing duration (in seconds) needed to trigger the debug output.",
+)
+
+
+@mod.action_class
+class Actions:
+
+    def debug_accessibility(el: Element = None):
+        """Prints information about the currently focused UI element to the terminal, for debugging"""
+
+        if not el:
+            el = ui.focused_element()
+
+        try:
+            # TODO(pcohen): make this work without Rich installed
+            from rich.console import Console
+            console = Console(color_system="truecolor", soft_wrap=True)
+
+            console.rule(f"{str(el)}'s attributes:")
+
+            # Attempt to sort the keys by relying on insertion order.
+            attributed = {}
+            for k in sorted(el.attrs):
+                attributed[k] = el.get(k)
+
+            console.print(attributed, markup=False)
+        except Exception as e:
+            print(f"Exception while debugging accessibility: \"{e}\":")
+            traceback.print_exc()
+
+
+active_hiss = {"cron": None}
+
+
+def hiss_over_threshold():
+    if not active_hiss.get("start"):
+        return False
+
+    return time.time() - active_hiss["start"] > setting_threshold.get()
+
+
+def stop_hiss():
+    trigger = hiss_over_threshold()
+
+    if active_hiss["cron"]:
+        cron.cancel(active_hiss["cron"])
+        active_hiss["cron"] = None
+
+    active_hiss["start"] = None
+
+    if trigger:
+        actions.user.debug_accessibility()
+
+
+def check_hiss():
+    if hiss_over_threshold():
+        stop_hiss()
+
+
+def start_hiss():
+    active_hiss["start"] = time.time()
+    active_hiss["cron"] = cron.interval("32ms", check_hiss)
+
+
+def on_hiss(noise_active: bool):
+    if not setting_enabled.get():
+        return
+
+    if noise_active:
+        start_hiss()
+    else:
+        stop_hiss()
+
+
+noise.register('hiss', on_hiss)
---
+++ dictation/electron.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from talon import Context, Module, actions, ui
+from talon.mac.ui import App
+from talon.ui import UIErr
+
+ctx = Context()
+ctx.matches = "os: mac"
+
+mod = Module()
+setting_electron_accessibility = mod.setting(
+    "enable_electron_accessibility",
+    type=bool,
+    default=False,
+    desc="Tells Electron apps to enable their accessibility trees, so that you can use accessibility dictation with them. Note that this could cause worse performance, depending on the app.",
+)
+
+@mod.action_class
+class ModActions:
+    def enable_electron_accessibility(app: Optional[App] = None):
+        """Enables AX support in Electron - may affect performance"""
+        if not app:
+            app = ui.active_app()
+        
+        try:
+            app.element.AXManualAccessibility = True
+        except UIErr:
+            # This will raise "Error setting element attribute" even on success.
+            pass
+
+
+def app_activate(app):
+    if setting_electron_accessibility.get():
+        actions.user.enable_electron_accessibility(app)
+
+ui.register("app_activate", app_activate)
